740f31eecc8611e5419791ca173027ef77ad23eb
AndreWake.art <andrewwake.art@gmail.com>
2020-07-27 16:54:20 +1100

    finalmente se añadio el que cada modal posee su respectiva imagen y al darle al boton aparece

--- indes.js
+++ indes.js
@@ -28,7 +28,7 @@ class animationJS{
 								  // timing options
 								  duration: this.duracion
 								});
-							setTimeout(function(){for(var i=0; i < elemento_A_Animar.length;i++){elemento_A_Animar[i].style.display="none";}}
+							setTimeout(function(){for(var i=0; i < elemento_A_Animar.length;i++){var elChild=elemento_A_Animar[i];var parent= elChild.parentNode; parent.removeChild(elChild)}}
 				,this.duracion);
 							}
 
@@ -42,12 +42,72 @@ class animationJS{
 								], { 
 								  // timing options
 								  duration: this.duracion
+							
+								});
+						}
+						if(this.nombre[i]== 'fadeinIn')
+						{
+
+						elemento_A_Animar[i].animate([
+						  // keyframes
+								  {  opacity: 0 ,transform: 'translateY(10px)',}, 
+								  { opacity: 1,transform: 'translateY(0px)',  }
+								], { 
+								  // timing options
+								  duration: this.duracion
 								});
 						}
 
 			}
 		}
 }
+
+
+class creacionDeElemento{
+	constructor(elemento,src,selector){
+		this.elemento = elemento;
+		this.src= src;
+		this.selector= selector;
+	}
+
+	crearModal(){
+		var div= document.createElement("div");
+		if(this.elemento == "modal"){
+			var content=document.querySelector(this.selector[0]);
+			div.classList.add('modal');
+			div.innerHTML=`
+				<div class="modal-content">
+			<div class="modal-head">
+				<div class="closeBtn" onclick="dissmiaseModal()">&times;</div>
+				<h1>hola soy un modal</h1>
+			</div>
+			<img src="${this.src[0]}" alt="">
+		
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas amet labore ad, sunt asperiores corporis ipsa, a alias ea et.</p>
+			</div>
+			`;
+			div.style.display="initial";
+			document.querySelector('.container').appendChild(div);
+			
+			console.log('se crea');
+			var animateJson={
+			selector:[],
+			nombre:[]
+	}
+			animateJson.selector=[".modal-content"];
+			animateJson.nombre=["fadeinIn"];
+			asignarAnimacion(animateJson.selector, animateJson.nombre, 1000);
+		}
+	}
+}
+
+
+
+
+
+
+
+
 		
 
 function asignarAnimacion(selector,nombre,duracion, tipoDeAnimacion){
@@ -107,6 +167,7 @@ var img=document.querySelectorAll('.contenedor');
 for(var v =0; v< img.length; v++){
 	img[v].addEventListener('mouseenter',function(){
 	var img=document.querySelectorAll('.img');
+
 for(var i=0; i< img.length ; i++){
 
 	var imgWidth= img[i].width;
@@ -155,10 +216,36 @@ function dissmiaseModal(){
 
 	animateJson.selector=[".modal-content"];
 	animateJson.nombre=["fadeout"];
-	console.log("hey");
 	asignarAnimacion(animateJson.selector, animateJson.nombre, 1000);
 	setTimeout(function(){
 		var modal=document.querySelector('.modal');
-		modal.style.display="none";
+		var parent= modal.parentNode;
+
+		parent.removeChild(modal);
 	},1000)
 }
+
+function crearModal(elemento,src,selector){
+	var CE= new creacionDeElemento(elemento,src,selector);
+	CE.crearModal();
+}
+
+function asignarModal(e){
+	var img= document.querySelector('#img1');
+	elementoModal.elemento=['modal'];
+	var srcElement= e.srcElement.parentNode.parentNode.children[1];
+	elementoModal.src=[srcElement.src];
+	elementoModal.selector=['#img1'];
+console.log(srcElement);
+	crearModal(elementoModal.elemento,elementoModal.src,elementoModal.selector);
+}
+var btnBg= document.querySelectorAll('.banB');
+for(var i = 0; i < btnBg.length; i++){
+	btnBg[i].addEventListener('click', asignarModal);
+}
+
+	var elementoModal={
+		elemento:[],
+		src:[],
+		selector:[]
+	};
