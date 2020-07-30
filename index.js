a2dacdcf6016e011c038fc70a29245162f0448ae
AndreWake.art <andrewwake.art@gmail.com>
2020-07-30 14:21:53 +1100

    migracion completa del proyecto a typescript, todo funcional pero se requerira agregar mas para el resto de funcionalidades

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 var controladorAnimationPlural = /** @class */ (function () {
-    function controladorAnimationPlural(selector, duracion, config, dA, interpolacion, keyFrameUno, keyFrameDos) {
+    function controladorAnimationPlural(selector, duracion, config, prefab, dA, interpolacion, keyFrameUno, keyFrameDos) {
         this.keyframeUno = keyFrameUno;
         this.keyframeDos = keyFrameDos;
         this.selector = selector;
         this.interpolacion = interpolacion;
         this.duracion = duracion;
         this.config = config;
+        this.prefab = prefab;
         this.diferentAnimation = dA;
     }
     controladorAnimationPlural.prototype.startAnimation = function () {
         if (this.config == 'individual') {
-            var selector = document.querySelector(this.selector);
-            selector.animate([this.keyframeUno.propUno, this.keyframeDos.propDos], this.duracion);
+            if (this.prefab == null) {
+                var selector = document.querySelector(this.selector);
+                selector.animate([this.keyframeUno.propUno, this.keyframeDos.propDos], this.duracion);
+            }
+            else {
+                var elemento_A_Animar = [];
+                for (var i = 0; i < this.selector.length; i++) {
+                    elemento_A_Animar[i] = document.querySelector(this.selector[i]);
+                    if (this.prefab == 'fadeout') {
+                        elemento_A_Animar[i].animate([
+                            // keyframes
+                            { opacity: 1, transform: 'translateY(0px)' },
+                            { opacity: 0, transform: 'translateY(10px)' }
+                        ], {
+                            // timing options
+                            duration: this.duracion
+                        });
+                    }
+                    if (this.prefab == 'fadein') {
+                        elemento_A_Animar[i].animate([
+                            // keyframes
+                            { opacity: 1, transform: 'translateY(0px)' },
+                            { opacity: 0, transform: 'translateY(-10px)' }
+                        ], {
+                            // timing options
+                            duration: this.duracion
+                        });
+                        setTimeout(function () { for (var i = 0; i < elemento_A_Animar.length; i++) {
+                            var elChild = elemento_A_Animar[i];
+                            var parent = elChild.parentNode;
+                            parent.removeChild(elChild);
+                        } }, this.duracion);
+                    }
+                    if (this.prefab == 'fadeinIn') {
+                        elemento_A_Animar[i].animate([
+                            // keyframes
+                            { opacity: 0, transform: 'translateY(10px)' },
+                            { opacity: 1, transform: 'translateY(0px)' }
+                        ], {
+                            // timing options
+                            duration: this.duracion
+                        });
+                    }
+                }
+            }
         }
         else if (this.config == 'plural') {
             if (this.diferentAnimation !== null) {
@@ -49,19 +93,144 @@ var controladorAnimationPlural = /** @class */ (function () {
     };
     return controladorAnimationPlural;
 }());
-var dA = {
-    selector: '',
-    animation: ''
-};
+var creacionDeElemento = /** @class */ (function () {
+    function creacionDeElemento(elemento, src, selector) {
+        this.elemento = elemento;
+        this.src = src;
+        this.selector = selector;
+    }
+    creacionDeElemento.prototype.crearModal = function () {
+        var div = document.createElement("div");
+        if (this.elemento == "modal") {
+            var content = document.querySelector(this.selector[0]);
+            div.classList.add('modal');
+            div.innerHTML = "\n\t\t\t\t<div class=\"modal-content\">\n\t\t\t<div class=\"modal-head\">\n\t\t\t\t<div class=\"closeBtn\" onclick=\"dissmiaseModal()\">&times;</div>\n\t\t\t\t\n\t\t\t</div>\n\t\t\t<img src=\"" + this.src[0] + "\" class=\"imgBan\" alt=\"\">\n\t\t\t<div class=\"modal-head\">\n\t\t\t<h1>hola soy un modal</h1>\n\t\t\t</div>\n\t\t\t<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas amet labore ad, sunt asperiores corporis ipsa, a alias ea et.</p>\n\t\t\t</div>\n\t\t\t";
+            div.style.display = "initial";
+            document.querySelector('.container').appendChild(div);
+            console.log('se crea');
+            //selector:any,duracion:number,config:string,prefab:string,dA?:dA,interpolacion?:string,keyFrameUno?:keyFrames,keyFrameDos?:keyFrames
+        }
+        asignarAnimacion(['.modal-content'], 1000, 'individual', 'fadeinIn');
+    };
+    return creacionDeElemento;
+}());
+var dm;
 var interactible = document.querySelectorAll('.dripple');
 for (var d = 0; d < interactible.length; d++) {
     interactible[d].addEventListener('click', function (e) {
-        var selectedAn = {
+        var dm = {
             selector: e.srcElement.parentNode.parentNode,
             animacion: 'fadeout'
         };
-        var animation = new controladorAnimationPlural(['.row-c'], 1000, 'plural', selectedAn, 'fadein');
+        var animation = new controladorAnimationPlural(['.row-c'], 1000, 'plural', 'fadein');
         animation.startAnimation();
     });
 }
-//selector:any,duracion:number,config:string,dA:dA,keyFrameUno?:keyFrames,keyFrameDos?:keyFrames,interpolacion?:string,
+//selector:any,duracion:number,config:string,dA?:dA,keyFrameUno?:keyFrames,keyFrameDos?:keyFrames,interpolacion?:string,
+//img,tiempo,individual,null,,linear,{},{}
+var img = document.querySelectorAll('.contenedor');
+for (var v = 0; v < img.length; v++) {
+    img[v].addEventListener('mouseenter', function () {
+        var img = document.querySelectorAll('.img');
+        for (var i = 0; i < img.length; i++) {
+            var imgWidth = img[i].width;
+            var imgHeight = img[i].height;
+            var imgfv = imgHeight / 2 - 20;
+            var imgfvf = imgHeight / 2;
+            var button = document.querySelectorAll(".banB");
+            button[i].animate([{ opacity: 0, transform: 'translateY(' + imgfv + 'px)' },
+                { opacity: 1, transform: 'translateY(' + imgfvf + 'px)' }], {
+                duration: 1000
+            });
+            button[i].style.transform = "translateY(" + imgHeight / 2 + "px)";
+        }
+    });
+}
+window.addEventListener('load', function () {
+    configurarBan();
+    document.querySelector('iframe').width = "100%";
+    document.querySelector('iframe').height = "500px";
+});
+window.addEventListener('resize', function () {
+    configurarBan();
+});
+window.addEventListener('scroll', function () {
+    var valorScroll = window.scrollY;
+    var navbar = document.querySelector('.navbar');
+    if (valorScroll >= 92) {
+        navbar.classList.add("damn");
+    }
+    if (valorScroll <= 92) {
+        navbar.classList.toggle('damn');
+    }
+});
+//selector:any,duracion:number,config:string,dA:dA,interpolacion?:string,keyFrameUno?:keyFrames,keyFrameDos?:keyFrames
+//creacion de modal, seleccion y eliminacion
+function asignarAnimacion(selector, duracion, config, prefab, dA, interpolacion, keyFrameUno, keyFrameDos) {
+    var animacion = new controladorAnimationPlural(selector, duracion, config, prefab, dA, interpolacion);
+    animacion.startAnimation();
+}
+function dissmiaseModal() {
+    asignarAnimacion([".modal-content"], 1000, 'individual', 'fadeout');
+    setTimeout(function () {
+        var modal = document.querySelector('.modal');
+        var parent = modal.parentNode;
+        parent.removeChild(modal);
+    }, 1000);
+}
+function crearModal(elemento, src, selector) {
+    var CE = new creacionDeElemento(elemento, src, selector);
+    CE.crearModal();
+}
+var animateJson = {
+    selector: [],
+    nombre: []
+};
+function asignarModal(e) {
+    var img = document.querySelector('#img1');
+    elementoModal.elemento = ['modal'];
+    var srcElement = e.srcElement.parentNode.parentNode.children[1];
+    elementoModal.src = [srcElement.src];
+    elementoModal.selector = ['#img1'];
+    console.log(srcElement);
+    crearModal(elementoModal.elemento, elementoModal.src, elementoModal.selector);
+}
+var btnBg = document.querySelectorAll('.banB');
+for (var i = 0; i < btnBg.length; i++) {
+    btnBg[i].addEventListener('click', asignarModal);
+}
+var elementoModal = {
+    elemento: [],
+    src: [],
+    selector: []
+};
+//ban
+function configurarBan() {
+    var ban = document.querySelectorAll('.ban');
+    var img = document.querySelectorAll('.img');
+    for (var i = 0; i < img.length; i++) {
+        var imgWidth = img[i].width;
+        var imgHeight = img[i].height;
+        ban[i].style.height = imgHeight + "px";
+        ban[i].style.width = imgWidth + "px";
+    }
+}
+var img = document.querySelectorAll('.contenedor');
+for (var v = 0; v < img.length; v++) {
+    img[v].addEventListener('mouseenter', function () {
+        var img = document.querySelectorAll('.img');
+        for (var i = 0; i < img.length; i++) {
+            var imgWidth = img[i].width;
+            var imgHeight = img[i].height;
+            var imgfv = imgHeight / 2 - 20;
+            var imgfvf = imgHeight / 2;
+            var button = document.querySelectorAll(".banB");
+            button[i].animate([{ opacity: 0, transform: 'translateY(' + imgfv + 'px)' },
+                { opacity: 1, transform: 'translateY(' + imgfvf + 'px)' }
+            ], {
+                duration: 1000
+            });
+            button[i].style.transform = "translateY(" + imgHeight / 2 + "px)";
+        }
+    });
+}
--- index.ts
+++ index.ts
@@ -17,7 +17,8 @@ class controladorAnimationPlural{
 	public duracion:number;
 	public config:string;
 	public diferentAnimation:dA;
-	constructor(selector:any,duracion:number,config:string,dA:dA,interpolacion?:string,keyFrameUno?:keyFrames,keyFrameDos?:keyFrames)
+	public prefab:string;
+	constructor(selector:any,duracion:number,config:string,prefab:string,dA?:dA,interpolacion?:string,keyFrameUno?:keyFrames,keyFrameDos?:keyFrames)
 		{
 		this.keyframeUno= keyFrameUno;
 		this.keyframeDos= keyFrameDos;
@@ -25,16 +26,68 @@ class controladorAnimationPlural{
 		this.interpolacion = interpolacion;
 		this.duracion=duracion;
 		this.config= config;
+		this.prefab=prefab;
 		this.diferentAnimation=dA;
 		}
 
 		startAnimation(){
 			if(this.config=='individual'){
-				const selector = document.querySelector(this.selector);
 
-				selector.animate([this.keyframeUno.propUno,this.keyframeDos.propDos],this.duracion);
+				if(this.prefab == null){
+					const selector = document.querySelector(this.selector);
+					selector.animate([this.keyframeUno.propUno,this.keyframeDos.propDos],this.duracion);
+				}
 
-			}else if(this.config=='plural'){
+				else{
+					var elemento_A_Animar = [];
+
+					for(var i=0; i < this.selector.length; i++)
+					{
+					elemento_A_Animar[i] = document.querySelector(this.selector[i]);
+					if(this.prefab=='fadeout'){
+
+						elemento_A_Animar[i].animate([
+						  // keyframes
+								  {  opacity: 1 ,transform: 'translateY(0px)',}, 
+								  { opacity: 0,transform: 'translateY(10px)',  }
+								], { 
+								  // timing options
+								  duration: this.duracion
+							
+								});
+					}if(this.prefab=='fadein'){
+							elemento_A_Animar[i].animate([
+							  // keyframes
+								  {  opacity: 1 ,transform: 'translateY(0px)',}, 
+								  { opacity: 0,transform: 'translateY(-10px)',  }
+								], { 
+								  // timing options
+								  duration: this.duracion
+								});
+							setTimeout(function(){for(var i=0; i < elemento_A_Animar.length;i++){var elChild=elemento_A_Animar[i];var parent= elChild.parentNode; parent.removeChild(elChild)}}
+				,this.duracion);
+					}
+					if(this.prefab=='fadeinIn')
+						{
+						elemento_A_Animar[i].animate([
+						  // keyframes
+								  {  opacity: 0 ,transform: 'translateY(10px)',}, 
+								  { opacity: 1,transform: 'translateY(0px)',  }
+								], { 
+								  // timing options
+								  duration: this.duracion
+								});
+						}
+					}
+				}
+				
+
+			}
+
+
+
+
+			else if(this.config=='plural'){
 				if(this.diferentAnimation !== null){
 					if(this.diferentAnimation.animacion == 'fadeout'){
 						this.diferentAnimation.selector.animate([
@@ -77,21 +130,216 @@ class controladorAnimationPlural{
 		}
 }
 
-var dA={
-	selector:'',
-	animation:''
-}
+
+class creacionDeElemento{
+	public elemento:any;
+	public src:any;
+	public selector:any;
+	constructor(elemento,src,selector){
+		this.elemento = elemento;
+		this.src= src;
+		this.selector= selector;
+	}
+
+	crearModal(){
+		var div= document.createElement("div");
+		if(this.elemento == "modal"){
+			var content=document.querySelector(this.selector[0]);
+			div.classList.add('modal');
+			div.innerHTML=`
+				<div class="modal-content">
+			<div class="modal-head">
+				<div class="closeBtn" onclick="dissmiaseModal()">&times;</div>
+				
+			</div>
+			<img src="${this.src[0]}" class="imgBan" alt="">
+			<div class="modal-head">
+			<h1>hola soy un modal</h1>
+			</div>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas amet labore ad, sunt asperiores corporis ipsa, a alias ea et.</p>
+			</div>
+			`;
+			div.style.display="initial";
+			document.querySelector('.container').appendChild(div);
+			
+			console.log('se crea');
+			//selector:any,duracion:number,config:string,prefab:string,dA?:dA,interpolacion?:string,keyFrameUno?:keyFrames,keyFrameDos?:keyFrames
+	}
+			asignarAnimacion(['.modal-content'],1000,'individual','fadeinIn');
+		}
+	}
+
+
+var dm:dA;
 
 var interactible=document.querySelectorAll('.dripple')
 for (var d= 0; d<interactible.length;d++){
 	interactible[d].addEventListener('click',function(e){
-		var selectedAn:dA = {
+		var dm:dA = {
 			selector:e.srcElement.parentNode.parentNode,
 			animacion:'fadeout'
 		}
-	var animation= new controladorAnimationPlural(['.row-c'],1000,'plural',selectedAn,'fadein');
+	var animation= new controladorAnimationPlural(['.row-c'],1000,'plural','fadein');
 	animation.startAnimation();
 })
 }
 
-//selector:any,duracion:number,config:string,dA:dA,keyFrameUno?:keyFrames,keyFrameDos?:keyFrames,interpolacion?:string,
+//selector:any,duracion:number,config:string,dA?:dA,keyFrameUno?:keyFrames,keyFrameDos?:keyFrames,interpolacion?:string,
+//img,tiempo,individual,null,,linear,{},{}
+
+
+
+
+var img=document.querySelectorAll('.contenedor');
+
+for(var v =0; v< img.length; v++)
+{
+	img[v].addEventListener('mouseenter',function()
+	{
+			var img=document.querySelectorAll('.img');
+
+		for(var i=0; i< img.length ; i++){
+
+			var imgWidth= img[i].width;
+			var imgHeight= img[i].height;
+			var imgfv=imgHeight/2 - 20;
+			var imgfvf=imgHeight/2;
+			var button=document.querySelectorAll(".banB");
+			button[i].animate([{  opacity: 0,transform: 'translateY('+ imgfv +'px)'}, 
+				{ opacity:1, transform:'translateY(' + imgfvf+ 'px)'}], { 
+											  duration:1000
+										});
+		button[i].style.transform="translateY("+imgHeight/2 +"px)";
+		}
+	}
+	
+						);
+}
+
+
+window.addEventListener('load', function(){
+	configurarBan();
+	document.querySelector('iframe').width="100%";
+		document.querySelector('iframe').height="500px";
+})
+window.addEventListener('resize', function(){
+	configurarBan();
+})
+
+window.addEventListener('scroll', function(){
+	var valorScroll= window.scrollY;
+
+	var navbar=document.querySelector('.navbar');
+
+	if (valorScroll >= 92)
+	{
+		navbar.classList.add("damn")
+	}
+	if (valorScroll <= 92)
+	{
+		navbar.classList.toggle('damn');
+	}
+})
+
+
+
+
+//selector:any,duracion:number,config:string,dA:dA,interpolacion?:string,keyFrameUno?:keyFrames,keyFrameDos?:keyFrames
+
+
+//creacion de modal, seleccion y eliminacion
+function asignarAnimacion(selector:any,duracion:number,config:string,prefab:string,dA?:dA,interpolacion?:string,keyFrameUno?:keyFrames,keyFrameDos?:keyFrames){
+	var animacion= new   controladorAnimationPlural(selector,duracion,config,prefab,dA,interpolacion);
+	animacion.startAnimation();
+}
+
+
+function dissmiaseModal(){
+	asignarAnimacion([".modal-content"],1000,'individual','fadeout');
+	setTimeout(function(){
+		var modal=document.querySelector('.modal');
+		var parent= modal.parentNode;
+
+		parent.removeChild(modal);
+	},1000)
+}
+
+function crearModal(elemento,src,selector){
+	var CE= new creacionDeElemento(elemento,src,selector);
+	CE.crearModal();
+}
+var animateJson={
+		selector:[],
+		nombre:[]
+	}
+function asignarModal(e){
+	var img= document.querySelector('#img1');
+
+	elementoModal.elemento=['modal'];
+	var srcElement= e.srcElement.parentNode.parentNode.children[1];
+	elementoModal.src=[srcElement.src];
+	elementoModal.selector=['#img1'];
+console.log(srcElement);
+	crearModal(elementoModal.elemento,elementoModal.src,elementoModal.selector);
+}
+
+var btnBg= document.querySelectorAll('.banB');
+
+		for(var i = 0; i < btnBg.length; i++){
+			btnBg[i].addEventListener('click', asignarModal);
+		}
+
+			var elementoModal={
+				elemento:[],
+				src:[],
+				selector:[]
+			};
+
+
+//ban
+	function configurarBan()
+	{
+
+	var ban=document.querySelectorAll('.ban');
+	var img=document.querySelectorAll('.img');
+
+	for(var i=0; i < img.length;i++)
+		{
+		var imgWidth= img[i].width;
+		var imgHeight= img[i].height;
+
+		ban[i].style.height=imgHeight +"px";
+		ban[i].style.width=imgWidth +"px";
+		}
+	}
+
+var img=document.querySelectorAll('.contenedor');
+
+for(var v =0; v< img.length; v++)
+{
+		img[v].addEventListener('mouseenter',function()
+	{
+		var img=document.querySelectorAll('.img');
+
+	for(var i=0; i< img.length ; i++)
+		{
+
+		var imgWidth= img[i].width;
+		var imgHeight= img[i].height;
+		var imgfv=imgHeight/2 - 20;
+		var imgfvf=imgHeight/2;
+		var button=document.querySelectorAll(".banB");
+		button[i].animate(
+				[	{  opacity: 0,transform: 'translateY('+ imgfv +'px)'}, 
+					{ opacity:1, transform:'translateY(' + imgfvf+ 'px)'}
+				], 
+				{ 
+					duration:1000
+				}
+			);
+		button[i].style.transform = "translateY("+imgHeight/2 +"px)";
+		}
+	}
+		
+	);
+}
